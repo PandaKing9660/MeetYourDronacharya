7c5b484e912447d1e91d184a3da998bff4afd316
AMITKESARI2000 <kesari.amit33@gmail.com>
2021-10-15 13:01:50 +0530

    icon add footer

--- client/src/Components/Home/Footer.js
+++ client/src/Components/Home/Footer.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 import Grid from '@mui/material/Grid';
@@ -6,10 +6,21 @@ import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 import { Link } from 'react-router-dom';
 import './footer.css';
 
 export default function Footer() {
+    const [socialLinks, setSocialLinks] = useState([
+        { name: 'Facebook', link: 'https://www.facebook.com', color: 'info', icon:<FacebookIcon/> },
+        { name: 'LinkedIn', link: 'https://www.lindein.com', color: 'primary', icon:<LinkedInIcon/> },
+        { name: 'Github', link: 'https://www.github.com', color: 'secondary', icon : <GitHubIcon/> },
+        { name: 'Twitter', link: 'https://www.twitter.com', color: 'info', icon:<TwitterIcon/>},
+    ]);
     return (
         <Box className="containerBox_footer" sx={{ width: '100%' }}>
             <Box sx={{ my: 1, mx: 2 }} className="head_footer">
@@ -22,10 +33,21 @@ export default function Footer() {
                     <Grid item>
                         <Typography gutterBottom variant="h6" component="div">
                             <Stack direction="row" spacing={1}>
-                                <Chip color="primary" label="LinkedIn" />
-                                <Chip color="info" label="Facebook" />
-                                <Chip color="success" label="Medium" />
-                                <Chip color="secondary" label="Github" />
+                                {socialLinks.map((socialLink) => {
+                                    return (
+                                        <Chip
+                                            color={socialLink.color}
+                                            label={socialLink.name}
+                                            component="a"
+                                            href={socialLink.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            clickable
+                                            variant="outlined"
+                                            icon={socialLink.icon}
+                                        ></Chip>
+                                    );
+                                })}
                             </Stack>
                         </Typography>
                     </Grid>
@@ -43,7 +65,7 @@ export default function Footer() {
                 {/* <Button>Add to cart</Button> */}
                 <div className="footerLinkContainer">
                     <div className="footerLinks">
-                        <Link to="/" >Home</Link>
+                        <Link to="/">Home</Link>
                         <Link to="/ask-something">Ask Something</Link>
                         <Link to="/experience">Experience</Link>
                         <Link to="/study-material">Study Material</Link>
