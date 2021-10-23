6ba6aa66c2c09bf2465c86575b4bfc16a8f477fb
pandaking <aditya9660sharma@gmail.com>
2021-10-23 23:42:15 +0530

    ask something backend implemented, filters added for questions, like dislike added both frontend and backend

--- server/models/askSomethingAnswer.js
+++ server/models/askSomethingAnswer.js
@@ -2,12 +2,15 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
 const askSomethingAnswerSchema = new Schema ({
+  title: {type: String, require: true},
   answer: {type: String, require: true},
   by: {type: String, required: true},
   to: {type: String, required: true},
   time: {type: Date, default: Date.now},
   liked: {type: [String], required: true},
   disliked: {type: [String], required: true},
+  userName: {type: String, required: true},
+  userImage: {type: String, required: true},
 });
 
 const askSomethingAnswer = mongoose.model (
