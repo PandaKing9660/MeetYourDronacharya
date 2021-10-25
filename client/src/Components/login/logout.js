c9844947090eee8d6142023f8858418d5a8cc66d
pandaking <aditya9660sharma@gmail.com>
2021-10-25 10:01:07 +0530

    logout added

--- client/src/Components/login/logout.js
+++ client/src/Components/login/logout.js
@@ -1,5 +1,8 @@
 import React, {useEffect} from 'react';
-import CircularProgress from '@mui/material/CircularProgress';
+import LinearProgress from '@mui/material/LinearProgress';
+import Stack from '@mui/material/Stack';
+import {Box} from '@mui/system';
+import {Typography} from '@mui/material';
 
 const Logout = () => {
   useEffect (() => {
@@ -10,26 +13,17 @@ const Logout = () => {
   }, []);
 
   return (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-      <CircularProgress />
-    </div>
+    <Box>
+      
+      <Stack sx={{width: '100%', color: 'grey.500'}} spacing={3} mt={4}>
+      <Typography variant="h4">
+  Signing You out...
+</Typography>
+        <LinearProgress color="secondary" />
+        <LinearProgress color="success" />
+        <LinearProgress color="inherit" />
+      </Stack>
+    </Box>
   );
 };
 
