f451238c27d2c18245bfecfce1675bbce831bb68
pandaking <aditya9660sharma@gmail.com>
2021-11-30 22:12:59 +0530

    bugs of asksomething, login and bookmarks rectified

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -27,7 +27,11 @@ const googleSuccess = async res => {
   // sending request to backend, incase of deployment using netlify else localhost for development
 
   axios
-    .post (`${process.env.REACT_APP_BACKEND_URL}/login`, {email, password, isGoogle})
+    .post (`${process.env.REACT_APP_BACKEND_URL}/login`, {
+      email,
+      password,
+      isGoogle,
+    })
     .then (res => {
       if (res.data.found === true) {
         // user found and saved in localStorage
@@ -102,7 +106,7 @@ const Login = () => {
   };
 
   const handleForgetPassword = () => {
-    if (email === '') {
+    if (!email.includes ('@')) {
       // inform to fill form
       alert ('Please fill email for verification');
     } else {
@@ -212,7 +216,10 @@ const Login = () => {
               </Grid>
               {/* If user forget password */}
               <Grid item onClick={handleForgetPassword}>
-                <Link href={email ? `/verify/${email}` : '/login'} variant="body2">
+                <Link
+                  href={email.includes ('@') ? `/verify/${email}` : '/login'}
+                  variant="body2"
+                >
                   forgot password ?
                 </Link>
               </Grid>
